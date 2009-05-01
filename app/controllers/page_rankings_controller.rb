887bf91378827f5cfb3eaf49446e3738929c12d4
peakpg <peakpg@gmail.com>
2009-05-01 11:44:41 -0400

    PageRank name is now calculated when adding to the database

--- app/controllers/page_rankings_controller.rb
+++ app/controllers/page_rankings_controller.rb
@@ -3,6 +3,7 @@ class PageRankingsController < ApplicationController
   def create
     @page_ranking = PageRanking.new(params[:page_ranking])
     @page_ranking.ip = request.remote_ip
+    @page_ranking.name = "Page '#{@page_ranking.page.name}' ranked as #{@page_ranking.rank}"
     if !@page_ranking.save
       flash[:notice] = "New Ranking was created"
     end
--- test/functional/page_rankings_controller_test.rb
+++ test/functional/page_rankings_controller_test.rb
@@ -1,8 +1,24 @@
-require 'test_helper'
+require File.join(File.dirname(__FILE__), '/../test_helper')
 
-class PageRankingControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class PageRankingsControllerTest < ActionController::TestCase
+
+  def setup
+
+  end
+  
+  test "Should create a ranking with a name" do
+    page = Page.create!(:name => "Rank Me", :path=>"/path")
+    assert_equal "0.0.0.0", @request.remote_ip, "Verifies what the expected IP is from the Mock request"
+
+    post :create, :page_ranking => {:rank => 4, :page_id => page.id}
+
+    ranks = PageRanking.all
+
+
+    assert_equal 1, ranks.size
+    assert_equal page, ranks[0].page
+    assert_equal 4, ranks[0].rank
+    assert_equal "0.0.0.0", ranks[0].ip
+    assert_equal "Page 'Rank Me' ranked as 4", ranks[0].name
   end
 end
